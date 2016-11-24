16e04715de6d88bf52c9fce1623649ea5acb14e9
Patrick Robin <hello@patrickrobin.co.uk>
2016-11-24 20:55:10 +0000

    Added SourceControlLibraries project

---
+++ Izio.Umbraco.SourceControlLibraries/Backup.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Web.Hosting;
+using System.Xml.Linq;
+using umbraco.interfaces;
+using Umbraco.Core;
+using Umbraco.Core.Logging;
+
+namespace Izio.Umbraco.SourceControlLibraries
+{
+    public class Backup : ApplicationEventHandler
+    {
+        protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication,
+            ApplicationContext applicationContext)
+        {
+            var installedPackagesConfig = XDocument.Load(HostingEnvironment.MapPath("~/App_Data/packages/installed/installedPackages.config"));
+            var installedLibraries = installedPackagesConfig.Descendants("file").Where(f => f.Value.ToLower().EndsWith("dll")).Select(f => f.Value.Substring(5)).Distinct();
+            var configuration = Configuration.GetConfiguration() ?? new Configuration {Folder = "Libraries"};
+
+            //create backup folder if it doesn't exist
+            if (Directory.Exists(HostingEnvironment.MapPath("~/" + configuration.Folder)) == false)
+            {
+                Directory.CreateDirectory(HostingEnvironment.MapPath("~/" + configuration.Folder));
+            }
+
+            //delete existing backups that are no longer required and remove from configuration
+            foreach (var library in Directory.GetFiles(HostingEnvironment.MapPath("~/" + configuration.Folder)))
+            {
+                var fileName = Path.GetFileName(library);
+
+                if (configuration.Libraries.Contains(fileName) == false)
+                {
+                    File.Delete(library);
+
+                    configuration.Libraries.Remove(fileName);
+                }
+            }
+
+            //iterate over all libraries
+            foreach (var library in installedLibraries)
+            {
+                if (File.Exists(HostingEnvironment.MapPath("~/bin/" + library)))
+                {
+                    //copy library to backup location
+                    File.Copy(HostingEnvironment.MapPath("~/bin/" + library), HostingEnvironment.MapPath("~/" + configuration.Folder + "/" + library), true);
+                }
+                else
+                {
+                    //restore library from backup location
+                    File.Copy(HostingEnvironment.MapPath("~/" + configuration.Folder + "/" + library), HostingEnvironment.MapPath("~/bin/" + library), true);
+
+                    //execute packages if specified
+                    if (configuration.ExecutePackages)
+                    {
+                        //load plugin assembly
+                        var plugin = System.Reflection.Assembly.LoadFile(HostingEnvironment.MapPath("~/bin/" + library));
+
+                        //create instances of all objects that implement IPackageAction
+                        var instances = plugin.GetTypes().Where(mytype => mytype.GetInterfaces().Contains(typeof(IPackageAction))).Select(t => Activator.CreateInstance(t) as IPackageAction);
+
+                        //iterate over all instances and execute 
+                        foreach (var instance in instances)
+                        {
+                            try
+                            {
+                                instance.Execute(instance.Alias(), instance.SampleXml());
+                            }
+                            catch (Exception ex)
+                            {
+                                LogHelper.Error<Backup>("Failed to execute package", ex);
+                            }
+                        }
+                    }
+                }
+
+                if (configuration.Libraries.Contains(library) == false)
+                {
+                    configuration.Libraries.Add(library);
+                }
+            }
+
+            //save configuration
+            configuration.Save();
+        }
+    }
+}
---
+++ Izio.Umbraco.SourceControlLibraries/Configuration.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Web.Hosting;
+using System.Xml.Serialization;
+using Umbraco.Core.Logging;
+
+namespace Izio.Umbraco.SourceControlLibraries
+{
+    [Serializable]
+    public class Configuration
+    {
+        public string Folder { get; set; }
+
+        public bool ExecutePackages { get; set; }
+
+        public List<string> Libraries { get; set; }
+
+        public Configuration()
+        {
+            Libraries = new List<string>();
+        }
+
+        public void Save()
+        {
+            try
+            {
+                var serializer = new XmlSerializer(typeof(Configuration));
+
+                using (var writer = new StreamWriter(HostingEnvironment.MapPath("~/Config/SourceControlLibraries.config")))
+                {
+                    serializer.Serialize(writer, this);
+                }
+            }
+            catch (Exception ex)
+            {
+                LogHelper.Error<Configuration>("Failed to save configuration file", ex);
+            }
+        }
+
+        public static Configuration GetConfiguration()
+        {
+            try
+            {
+                var serializer = new XmlSerializer(typeof(Configuration));
+
+                using (var reader = File.OpenText(HostingEnvironment.MapPath("~/Config/SourceControlLibraries.config")))
+                {
+                    return (Configuration)serializer.Deserialize(reader);
+                }
+            }
+            catch (Exception ex)
+             {
+                LogHelper.Error<Configuration>("Failed to load configuration file", ex);
+            }
+
+            return null;
+        }
+    }
+}
